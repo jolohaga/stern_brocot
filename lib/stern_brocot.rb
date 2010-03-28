8ff05f0378b037e47d01dc00b51ee628e6816b56
Jose Hales-Garcia <jolohaga@me.com>
2010-03-28 00:49:53 -0700

    Initial commit. 	new file:   History.txt 	new file:   Manifest 	new file:   README.txt 	new file:   Rakefile 	new file:   lib/stern_brocot.rb 	new file:   spec/spec_helper.rb 	new file:   spec/stern_brocot/stern_brocot_spec.rb

---
+++ lib/stern_brocot.rb
@@ -0,0 +1,14 @@
+# SternBrocot
+#
+# Generate the Stern-Brocot tree and series.
+#
+# Requires Ruby 1.9+
+#
+# Author:: Jose Hales-Garcia (mailto:jolohaga@me.com)
+# Copyright:: Copyright (c) 2010 Jose Hales-Garcia
+#
+#
+module SternBrocot
+  class SternBrocot
+  end
+end
