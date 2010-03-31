fbdc7e3d091826d318860db6544dc1d40864c9fc
Jose Hales-Garcia <jolohaga@me.com>
2010-03-30 22:46:30 -0700

    Initial dev. Created the classes Tree and Fraction. Added the + and to_* methods to the Fraction class.
    	modified:   lib/stern_brocot.rb
    	deleted:    spec/stern_brocot/stern_brocot_spec.rb
    	new file:   spec/stern_brocot_spec.rb

--- lib/stern_brocot.rb
+++ lib/stern_brocot.rb
@@ -1,6 +1,6 @@
 # SternBrocot
 #
-# Generate the Stern-Brocot tree and series.
+# Generate Stern-Brocot fractions, trees and series.
 #
 # Requires Ruby 1.9+
 #
@@ -9,6 +9,36 @@
 #
 #
 module SternBrocot
-  class SternBrocot
+  class Tree
+    def initialize(start, depth=0)
+    end
+  end
+  
+  class Fraction
+    attr_accessor :numerator, :denominator
+    
+    def initialize(numerator=0, denominator=1)
+      @numerator=numerator
+      @denominator=denominator
+    end
+    
+    def +(other)
+      return SternBrocot::Fraction.new(self.numerator + other.numerator, self.denominator + other.denominator)
+    end
+    
+    def to_s
+      return "#{numerator}/#{denominator}"
+    end
+    
+    def to_a
+      return [numerator,denominator]
+    end
+    
+    def to_r
+      return Rational(numerator,denominator)
+    end
+  end
+  
+  class << self
   end
 end
--- spec/stern_brocot/stern_brocot_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.join(File.dirname(__FILE__), "/../spec_helper")
-
-module SternBrocot
-  describe SternBrocot do
-  end
-end
